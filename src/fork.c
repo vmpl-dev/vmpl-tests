6c75c0e7b5a143cfc6ae12d1b1437b82fdfd8ec7
Benshan Mei <mbs2021@163.com>
2023-12-14 03:30:42 +0800

    64%: Checks: 28, Failures: 4, Errors: 6 src/benchmark.c:84:P:Core:test_process:0: Passed src/benchmark.c:133:P:Core:test_sys:0: Passed src/benchmark.c:227:F:Core:test_prctl:0: Assertion 'gs_reg_value != 0' failed: gs_reg_value == 0, 0 == 0 src/benchmark.c:240:P:Core:test_rdtsc:0: Passed src/benchmark.c:235:P:Core:test_syscall:0: Passed src/benchmark.c:180:P:Core:test_vsyscall:0: Passed src/benchmark.c:665:P:Core:test_fork:0: Passed src/benchmark.c:691:E:Core:test_vfork:0: (after this point) Test timeout expired src/benchmark.c:725:E:Core:test_pthread:0: (after this point) Received signal 11 (Segmentation fault) src/benchmark.c:323:P:Core:test_mmap:0: Passed src/benchmark.c:343:F:Core:test_mprotect:0: Assertion 'addr != ((void *) -1)' failed: addr == 0xffffffff, ((void *) -1) == 0xffffffff src/benchmark.c:356:F:Core:test_munmap:0: Assertion 'addr != ((void *) -1)' failed: addr == 0xffffffff, ((void *) -1) == 0xffffffff src/benchmark.c:431:F:Core:test_sbrk:0: Assertion 'tmp_brk != (void *)-1' failed: tmp_brk == 0xffffffff, (void *)-1 == 0xffffffff src/benchmark.c:487:P:Core:test_pipe:0: Passed src/benchmark.c:465:E:Core:test_sem:0: (after this point) Received signal 11 (Segmentation fault) src/benchmark.c:604:P:Core:test_msg:0: Passed src/benchmark.c:527:E:Core:test_shm:0: (after this point) Test timeout expired src/benchmark.c:194:P:Core:test_signal:0: Passed src/benchmark.c:190:P:Core:test_vdso:0: Passed src/benchmark.c:258:P:Core:test_time_vdso:0: Passed src/benchmark.c:275:E:Core:test_time_syscall:0: (after this point) Test timeout expired src/benchmark.c:293:E:Core:test_time:0: (after this point) Test timeout expired src/test_mxml.c:24:P:Core:test_mxml_write:0: Passed src/test_mxml.c:48:P:Core:test_mxml_read:0: Passed src/test_mxml.c:76:P:Core:test_mxml:0: Passed src/test_zlib.c:55:P:zlib:test_zlib_inflate:0: Passed src/test_zlib.c:58:P:zlib:test_zlib:0: Passed src/benchmark.c:364:P:Core:test_seimi:0: Passed

--- src/fork.c
+++ src/fork.c
@@ -1,104 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <stddef.h>
+/* CELEBW02
+
+   The following function suspends the calling process using &waitpid.
+   until a child process ends.
+
+ */
+#define _POSIX_SOURCE
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 #include <pthread.h>
-#include <sys/prctl.h>
 
-#define ARCH_GET_FS 0x1003
-extern int arch_prctl(int code, unsigned long *addr);
-
-struct regs_t {
-	uint16_t cs;
-	uint16_t ds;
-	uint16_t es;
-	uint16_t fs;
-	uint16_t gs;
-	uint16_t ss;
-};
-
-static void get_fs_base(char *name)
-{
-	unsigned long fs_base0, fs_base1, fs_base2;
-
-	if (arch_prctl(ARCH_GET_FS, &fs_base0) == -1) {
-		return;
-	}
-
-	__asm__ volatile("mov %%fs:0, %0\n" : "=r"(fs_base1));
-	__asm__ volatile("rdfsbase %0\n" : "=r"(fs_base2));
-    if (fs_base0!= fs_base1 || fs_base0!= fs_base2) {
-		printf("%s fs_base0: %lx, fs_base1: %lx, fs_base2: %lx", name, fs_base0, fs_base1, fs_base2);
-		printf("%s fs_base0!= fs_base1 || fs_base0!= fs_base2", name);
-	} else {
-        printf("%s fs_base=%lx", name, fs_base0);
+int main(int argc, char *argv[]) {
+  pid_t pid;
+  time_t t;
+  int status;
+  int oldstate;
+	// disable cancel
+  if ((pid = fork()) < 0)
+    perror("fork() error");
+  else if (pid == 0) {
+		printf("child pid = %d\n", getpid());
+	  sleep(5);
+	  exit(1);
+  }
+  else do {
+    if ((pid = waitpid(pid, &status, WNOHANG)) == -1)
+      perror("wait() error");
+    else if (pid == 0) {
+      time(&t);
+      printf("child is still running at %s", ctime(&t));
+      sleep(1);
     }
-}
+    else {
+      if (WIFEXITED(status))
+        printf("child exited with status of %d\n", WEXITSTATUS(status));
+	  else if (WIFSIGNALED(status))
+		printf("child terminated abnormally, signal %d\n", WTERMSIG(status));
+	  else if (WIFSTOPPED(status))
+		  printf("child stopped, signal %d\n", WSTOPSIG(status));
+      else printf("child did not exit successfully with exit status %d\n", status);
+    }
+  } while (pid == 0);
 
-static void get_segment_registers(struct regs_t *regs)
-{
-	__asm__ volatile(
-        "movw %%cs, %c[cs](%0)\n"
-        "movw %%ds, %c[ds](%0)\n"
-        "movw %%es, %c[es](%0)\n"
-        "movw %%fs, %c[fs](%0)\n"
-        "movw %%gs, %c[gs](%0)\n"
-        "movw %%ss, %c[ss](%0)\n" ::"r"(regs),
-        [cs] "i"(offsetof(struct regs_t, cs)),
-        [ds] "i"(offsetof(struct regs_t, ds)),
-        [es] "i"(offsetof(struct regs_t, es)),
-        [fs] "i"(offsetof(struct regs_t, fs)),
-        [gs] "i"(offsetof(struct regs_t, gs)),
-        [ss] "i"(offsetof(struct regs_t, ss)));
-}
-
-#if 0
-static void do_busy_loop() {
-	// write a busy loop never be optimized out
-	// sleep for 1s
-	__asm__ volatile(
-		"mov $1000000000, %%ecx\n"  // change eax to ecx
-		"1:\n"
-		"loop 1b\n"
-		:
-		:
-		: "ecx"  // change eax to ecx
-	);
-}
-#else 
-void do_busy_loop(const char *name)
-{
-	struct regs_t regs;
-	// get_fs_base(name);
-	get_segment_registers(&regs);
-	printf("%s cs=%x ds=%x es=%x fs=%x gs=%x ss=%x\n", name,
-		   regs.cs, regs.ds, regs.es, regs.fs, regs.gs, regs.ss);
-}
-#endif
-
-int main(int argc, char *argv[])
-{
-	int r, status;
-	do_busy_loop("main");
-	int p = fork();
-	if (p < 0) {
-		printf("fork error: %d\n", p);
-		return 1;
-	} else if (p == 0) {
-		printf("in child\n");
-		do_busy_loop("child");
-		// r = usleep(1000);
-		// printf("child done sleeping, usleep=%d, errno=%d\n", r, errno);
-	} else {
-		printf("in parent, child=%d, errno=%d\n", p, errno);
-		do_busy_loop("parent");
-		r = usleep(1000);
-		printf("parent done sleeping, usleep=%d, errno=%d\n", r, errno);
-		r = waitpid(p, &status, 0);
-		printf("done waiting, waitpid=%d, errno=%d\n", r, errno);
-	}
-	return 0;
+  return 0;
 }
